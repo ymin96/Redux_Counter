2b3c88e14d3c3bb7c10b73bf88f49357aa4b3352
Kim YongHwan <ymin96@naver.com>
2020-05-06 16:45:56 +0900

    redux practice

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
+//리덕스 관련 불러오기
+import {createStore} from 'redux';
+import reducers from './reducers';
+import {Provider} from 'react-redux';
+
+//스토어 생성
+const store = createStore(reducers, window.devToolsExtension && window.devToolsExtension());
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
